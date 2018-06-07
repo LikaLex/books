7912767c18757c0e9ef66dce455ad00a01cf2ce4
Lika Lex <lika.lex555@gmail.com>
2018-06-07 18:23:29 +0300

    Add avatar to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+  #before_action :authenticate_user!, only: [ :edit]
+
+
+  def index
+      @users = User.all.order("created_at DESC").paginate(page: params[:page], per_page:4)
+  end
+
+  def edit
+
+  end
+
+
+  def update
+
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+
+  end
   def show
     @user  = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:last_name, :avatar)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :books
   has_many :reviews
+
+  has_attached_file :avatar, :styles =>  {:user_index => "250x350>",
+                                          :user_show => "325x475>" , :user_comment => "100x100>"  },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
 end
