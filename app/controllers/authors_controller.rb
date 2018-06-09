9b9792828e6e47bd268d3bf7bc532c7c417090e3
Lika Lex <lika.lex555@gmail.com>
2018-06-09 09:23:07 +0300

    Add links between pages

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,7 @@ class AuthorsController < ApplicationController
 
 
   def index
-    @authors = Author.all.order("created_at DESC").paginate(page: params[:page], per_page:4)
+    @authors = Author.all.order("created_at DESC").paginate(page: params[:page], per_page:8)
   end
 
   def new
