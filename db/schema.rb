9c6239a3b734bbbe3ef0a6e057864d8002653d22
Lika Lex <lika.lex555@gmail.com>
2018-07-03 11:07:47 +0300

    Add the missing gender to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_122129) do
+ActiveRecord::Schema.define(version: 2018_07_03_075725) do
 
   create_table "ages", force: :cascade do |t|
     t.string "name"
@@ -47,30 +47,10 @@ ActiveRecord::Schema.define(version: 2018_06_20_122129) do
     t.integer "number_of_pages"
     t.integer "author_id"
     t.integer "publisher_id"
-    t.string "slug"
     t.integer "age_id"
-    t.index ["slug"], name: "index_books_on_slug", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-  end
-
-  create_table "genders", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -89,8 +69,6 @@ ActiveRecord::Schema.define(version: 2018_06_20_122129) do
     t.text "country"
     t.text "ceo"
     t.text "founder"
-    t.string "slug"
-    t.index ["slug"], name: "index_publishers_on_slug", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -122,11 +100,11 @@ ActiveRecord::Schema.define(version: 2018_06_20_122129) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text "user_about"
+    t.string "provider"
+    t.string "uid"
     t.integer "gender_id"
-    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug"
   end
 
 end
